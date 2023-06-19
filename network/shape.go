66e539f7ada421e9d3a6e76c8fc951193dc1f7a1
Aron Jalbuena <aron@taubyte.com>
2023-06-19 14:48:40 -0500

    changes to config

--- network/shape.go
+++ network/shape.go
@@ -1,10 +1,6 @@
 package network
 
 type Shape struct {
-	Protocols []string `yaml:",omitempty"`
-	Ports     Ports    `yaml:",omitempty"`
-}
-
-type Ports struct {
-	P2P map[string]int `yaml:"p2p"`
+	Protocols []string
+	Ports     map[string]int `yaml:",omitempty"`
 }
